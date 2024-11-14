a0ef4b6fe824738e2bd81b25c337cc2ab83cf9b4
Charly3012 <carlocoronadosilva975@gmail.com>
2024-11-13 20:00:49 -0600

    Validation with JWT first preview

--- src/main/resources/db/migration/V1__createTableUsers.sql
+++ src/main/resources/db/migration/V1__createTableUsers.sql
@@ -1,13 +1,13 @@
 create table users
 (
 
-    id       bigint       not null auto_increment,
+    idUser bigint not null auto_increment,
     name   varchar(100) not null,
     nickname varchar(50)  not null unique,
     email    varchar(50)  not null unique,
     password varchar(200) not null,
     bornDate date,
 
-    primary key (id)
+    primary key (idUser)
 
 );
