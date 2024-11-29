94aede7545a60218c5db48a0b36d74b04f1a7ce9
JazDzib <a22216071@alumnos.uady.mx>
2024-11-29 03:04:13 -0600

    Creation of CRUD for user

--- src/main/java/com/wellnest/wellnest/Controller/UserController.java
+++ src/main/java/com/wellnest/wellnest/Controller/UserController.java
@@ -1,12 +1,16 @@
 package com.wellnest.wellnest.Controller;
 
+import com.wellnest.wellnest.Models.Request.User.ModifyUserProfile;
+import com.wellnest.wellnest.Models.Responses.ProfileResponseDTO;
 import com.wellnest.wellnest.Models.Responses.User.ProfileResponseDTO;
 import com.wellnest.wellnest.Models.User;
 import com.wellnest.wellnest.Repository.UserRepository;
 import com.wellnest.wellnest.Services.JwtService;
 import com.wellnest.wellnest.Services.UserService;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -26,6 +30,15 @@ public class UserController {
     @Autowired
     private JwtService jwtService;
 
+    @GetMapping(value = "getUsers")
+    public List<User> getUsers()
+    {
+        List<User> users;
+
+        users = userRepository.findAll();
+        return users;
+    }
+
     @GetMapping("getProfile")
     public ResponseEntity<ProfileResponseDTO> getCurrentUser(@RequestHeader("Authorization") String token){
         var user = userService.getUserFromToken(token);
@@ -37,6 +50,19 @@ public class UserController {
                                     @PathVariable int idUser){
         userService.addFollow(token, idUser);
         return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("{userId}")
+    public ResponseEntity<ProfileResponseDTO> deletUser(@RequestHeader("Authorization") String token, @PathVariable Long userId){
+        userService.deletUser(token,userId);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("{userId}")
+    @Transactional
+    public  ResponseEntity<ProfileResponseDTO> modifyUserProfile(@RequestHeader("Authorization") String token, @RequestBody ModifyUserProfile modifyRequest){
+        ProfileResponseDTO profileResponse = userService.modifyUser(token, modifyRequest);
+        return ResponseEntity.ok(profileResponse);
     }
 
 
